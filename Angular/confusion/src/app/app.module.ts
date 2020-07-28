b15348cfd7c1388f4db95253fc47ccd347efb9ab
Shubham Gandhi <shubhamrgandhi@gmail.com>
2020-07-28 12:50:43 +0530

    Directives

--- Angular/confusion/src/app/app.module.ts
+++ Angular/confusion/src/app/app.module.ts
@@ -41,6 +41,7 @@ import { LoginComponent } from './login/login.component';
 
 
 import { baseURL } from './shared/baseurl';
+import { HighlightDirective } from './directives/highlight.directive';
 
 @NgModule({
   declarations: [
@@ -52,7 +53,8 @@ import { baseURL } from './shared/baseurl';
     AboutComponent,
     HomeComponent,
     ContactComponent,
-    LoginComponent
+    LoginComponent,
+    HighlightDirective
   ],
   imports: [
     BrowserModule,
