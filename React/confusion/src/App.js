e4d76c897222db79e355537c2a232fdd31a1b215
Shubham Gandhi <shubhamrgandhi@gmail.com>
2020-08-09 16:59:22 +0530

    components part 1

--- React/confusion/src/App.js
+++ React/confusion/src/App.js
@@ -1,18 +1,22 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Menu from './components/MenuComponent'; 
 import { Navbar, NavbarBrand } from 'reactstrap' ;
 
-function App() {
-  return (
-    <div className="App">
-      <Navbar dark color="primary">
-        <div className="container">
-          <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
-        </div>
-      </Navbar>
-    </div>
-  );
+class App extends Component {
+  render() {  
+    return (
+      <div>
+        <Navbar dark color="primary">
+          <div className="container">
+            <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
+          </div>
+        </Navbar>
+        <Menu />
+      </div>
+    );
+  }  
 }
 
 export default App;
