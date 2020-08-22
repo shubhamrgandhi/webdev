eb1397e7df5880070811ca0dcb4ec8d763a7566c
Shubham Gandhi <shubhamrgandhi@gmail.com>
2020-08-22 12:52:56 +0530

    React Animations

--- React/confusion/src/redux/ActionCreators.js
+++ React/confusion/src/redux/ActionCreators.js
@@ -41,8 +41,8 @@ export const postComment = (dishId, rating, author, comment) => (dispatch) => {
     })
     .then(response => response.json())
     .then(response => dispatch(addComment(response)))
-    .catch(error => { console.log('Post comments', error.message) 
-        alert('Your comment could not be posted\nError: ' + error.message)});
+    .catch(error => { console.log('Post comments', error.message); 
+        alert('Your comment could not be posted\nError: ' + error.message);});
 }
 
 export const fetchDishes = () => (dispatch) => {
